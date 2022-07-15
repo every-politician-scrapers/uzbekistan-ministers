a8afcd24db476a0069d336cf0c73548c58509287
Tony Bowden <tony@tmtm.com>
2022-07-15 18:56:25 +0300

    move ruwiki scraper to sources/

--- bin/compare-ruwiki.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bundle exec ruby bin/scraper/wikipedia.rb > data/wikipedia.csv
-wd sparql -f csv bin/scraper/wikidata-wp.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > data/wikidata-wp.csv
-bundle exec ruby bin/wikipedia-diff.rb | tee data/wikipedia-diff.csv
---
+++ sources/ruwiki/compare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+bundle exec ruby scraper.rb > scraped.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
+bundle exec ruby diff.rb | tee diff.csv
+
+cd ~-
