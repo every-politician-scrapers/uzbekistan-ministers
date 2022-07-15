e418fa65edf4c34a9b9913efdc34a93085da6cb6
Tony Bowden <tony@tmtm.com>
2022-07-15 14:48:27 +0300

    less verbose comparisons

--- bin/compare-ruwiki.sh
+++ bin/compare-ruwiki.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/wikipedia.rb | ifne tee data/wikipedia.csv
-wd sparql -f csv bin/scraper/wikidata-wp.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee data/wikidata-wp.csv
+bundle exec ruby bin/scraper/wikipedia.rb > data/wikipedia.csv
+wd sparql -f csv bin/scraper/wikidata-wp.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > data/wikidata-wp.csv
 bundle exec ruby bin/wikipedia-diff.rb | tee data/wikipedia-diff.csv
