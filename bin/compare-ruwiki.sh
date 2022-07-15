78a8b8c18b2710daf9250378b232e4540faad231
Tony Bowden <tony@tmtm.com>
2022-07-15 14:48:27 +0300

    silence cd -

--- bin/compare-ruwiki.sh
+++ bin/compare-ruwiki.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/wikipedia.rb |
-  ifne tee data/wikipedia.csv
-
-wd sparql -f csv bin/scraper/wikidata-wp.js |
-  sed -e 's/T00:00:00Z//g' |
-  qsv dedup -s psid |
-  ifne tee data/wikidata-wp.csv
-
-bundle exec ruby bin/wikipedia-diff.rb |
-  tee data/wikipedia-diff.csv
+bundle exec ruby bin/scraper/wikipedia.rb | ifne tee data/wikipedia.csv
+wd sparql -f csv bin/scraper/wikidata-wp.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee data/wikidata-wp.csv
+bundle exec ruby bin/wikipedia-diff.rb | tee data/wikipedia-diff.csv
